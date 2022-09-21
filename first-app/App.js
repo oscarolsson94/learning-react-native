3ac1d2f8f295dfd61c3ea922d10ea2dd894fc124
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-09-21 11:29:06 +0200

    testing basic components

--- first-app/App.js
+++ first-app/App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, Linking } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Oscar");
+
   return (
     <View style={styles.body}>
       <Text>Testing react native</Text>
+      <Button
+        title="Link to page"
+        onPress={() => {
+          Linking.openURL("https://www.dn.se/");
+        }}
+      />
     </View>
   );
 }
