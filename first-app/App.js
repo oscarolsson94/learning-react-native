fdd9089e74648aaf138f7fe7dc872c1bb9b18f98
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-09-22 10:19:16 +0200

    testing some functionality

--- first-app/App.js
+++ first-app/App.js
@@ -1,8 +1,37 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, View, Linking } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+  FlatList,
+  RefreshControl,
+} from "react-native";
 
 export default function App() {
+  const listOfNames = [
+    "Name1",
+    "Name1",
+    "Name1",
+    "Name1",
+    "Name1",
+    "Name1",
+    "Name1",
+    "Name1",
+    "Name1",
+    "Name1",
+  ];
   const [name, setName] = useState("Oscar");
+  const [refreshing, setRefreshing] = useState(false);
+  const [itemsToDisplay, setItemsToDisplay] = useState(listOfNames);
+
+  const refreshHandler = () => {
+    setRefreshing(true);
+    setItemsToDisplay((itemsToDisplay) => [...itemsToDisplay, "New Name"]);
+
+    setRefreshing(false);
+  };
 
   const onClickHandler = () => {
     setName("Another name");
@@ -10,7 +39,14 @@ export default function App() {
 
   return (
     <View style={styles.body}>
-      <Text>My name is {name}</Text>
+      <FlatList
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={refreshHandler} />
+        }
+        data={itemsToDisplay}
+        renderItem={(name) => <Text style={styles.text}>{name.item}</Text>}
+      />
+      <Text>My name is not {name}</Text>
       <Button title="Change name" onPress={onClickHandler} />
     </View>
   );
@@ -22,5 +58,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    margin: 10,
   },
 });
